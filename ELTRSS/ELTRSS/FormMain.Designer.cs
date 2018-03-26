1742d5040b2f858473829e135d1b9cc2a9145090
liuzhitie <elite_liuzhitie@163.com>
2018-03-26 11:35:39 +0800

    add all source code files

---
+++ ELTRSS/ELTRSS/FormMain.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ELTRSS
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_button_test_sqlite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // m_button_test_sqlite
+            // 
+            this.m_button_test_sqlite.Location = new System.Drawing.Point(60, 60);
+            this.m_button_test_sqlite.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.m_button_test_sqlite.Name = "m_button_test_sqlite";
+            this.m_button_test_sqlite.Size = new System.Drawing.Size(175, 52);
+            this.m_button_test_sqlite.TabIndex = 1;
+            this.m_button_test_sqlite.Text = "Test SQLite";
+            this.m_button_test_sqlite.UseVisualStyleBackColor = true;
+            this.m_button_test_sqlite.Click += new System.EventHandler(this.m_button_test_sqlite_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 603);
+            this.Controls.Add(this.m_button_test_sqlite);
+            this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "FormMain";
+            this.Text = "Elite_Rack_Shuttle_System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button m_button_test_sqlite;
+    }
+}
+
---
+++ ELTRSS/ELTRSS/FormMain.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Data.SQLite;
+
+namespace ELTRSS
+{
+    public partial class FormMain : Form
+    {
+
+        #region 成员变量
+
+        //数据库连接
+        SQLiteConnection m_dbConnection;
+
+        // 文件名称
+        String m_strDBFileName;
+        String m_strDBCnnName;
+
+        #endregion 成员变量
+
+
+        public FormMain()
+        {
+            this.m_strDBFileName = @"d:\sqlitedata\elite.sqlite.user.data.db";
+            this.m_strDBCnnName = @"Data Source=d:\sqlitedata\elite.sqlite.user.data.db;Version=3;";
+
+            InitializeComponent();
+        }
+
+        private void m_button_test_sqlite_Click(object sender, EventArgs e)
+        {
+            createNewDatabase();
+            connectToDatabase();
+            createTable();
+            fillTable();
+            printHighscores();
+        }
+
+        #region 数据库访问函数
+
+
+        //创建一个空的数据库
+        void createNewDatabase()
+        {
+            // 删除原有的数据库文件
+            if (File.Exists(this.m_strDBFileName))
+                File.Delete(this.m_strDBFileName);
+
+            SQLiteConnection.CreateFile(this.m_strDBFileName);
+        }
+
+        //创建一个连接到指定数据库
+        void connectToDatabase()
+        {
+            m_dbConnection = new SQLiteConnection(m_strDBCnnName);
+            m_dbConnection.Open();
+        }
+
+        //在指定数据库中创建一个table
+        void createTable()
+        {
+            string sql = "create table user(name varchar(20), code int)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        //插入一些数据
+        void fillTable()
+        {
+            string sql = "insert into user (name, code) values ('LiuXuan', 1)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into user (name, code) values ('WeiHongyu', 2)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into user (name, code) values ('LiuXianghua', 3)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into user (name, code) values ('WangShengliang', 4)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+        }
+
+        //使用sql查询语句，并显示结果
+        void printHighscores()
+        {
+            string sql = "select * from user order by name desc";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                MessageBox.Show("Name: " + reader["name"] + "\tCode: " + reader["code"]);
+
+        }
+
+        #endregion 数据库访问函数
+    }
+}
